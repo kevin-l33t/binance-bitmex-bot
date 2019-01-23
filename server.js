6760b96811152d533ca89cde7937d9dda9d36911
Nicholas Stobie <nicholasstobie@Nicholass-MacBook-Pro.local>
2019-01-23 10:21:46 -0800

    troublishooting server

--- server.js
+++ server.js
@@ -358,7 +358,7 @@ app.post('/trade_notification', function(req, res) {
 });
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.listen(server_port, server_ip_address, function () {
 	console.log('BitMEX leverage bot is listening on ' + server_ip_address + " port: " + server_port);
