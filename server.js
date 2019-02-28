21ce2a191194ad51379b27d1c918eb3bbb15039a
Nicholas Stobie <nicholasstobie@Nicholass-MacBook-Pro.local>
2019-02-28 12:29:54 -0800

    fixed bugs causing too many requests

--- server.js
+++ server.js
@@ -52,25 +52,22 @@ app.post('/trade_notification', function(req, res) {
   if (tradeNotification.includes('bitmex')) {
     if (tradeNotification.includes('buy')) {
       for (var i=0; i<bitmexPairs.length; i++) {
+        console.log(bitmexPairs[i].toLowerCase(), tradeNotification)
         if (tradeNotification.includes(bitmexPairs[i].toLowerCase())) {
           console.log('bitmex buy order')
           bitmexBuyOrder(bitmexPairs[i], process.env.RETRY)
-        } else {
-          if (i === bitmexPairs.length - 1) {
-            email.sendTextErrorEmail("Bitmex bot could not identify a pair to buy or sell based on the text message. Make sure each text includes a pair string WITHOUT a '/', like so 'ethpax' (not case sensitive). Here is the message you sent: \n" + tradeNotification)
-          }
+          return
         }
+        email.sendTextErrorEmail("Bitmex bot could not identify a pair to buy or sell based on the text message. Make sure each text includes a pair string WITHOUT a '/', like so 'ethpax' (not case sensitive). Here is the message you sent: \n" + tradeNotification)
       }
     } else if (tradeNotification.includes('sell')) {
       for (var i=0; i<bitmexPairs.length; i++) {
         if (tradeNotification.includes(bitmexPairs[i].toLowerCase())) {
           console.log('bitmex sell order')
           bitmexSellOrder(bitmexPairs[i], process.env.RETRY)
-        } else {
-          if (i === bitmexPairs.length - 1) {
-            email.sendTextErrorEmail("Bitmex bot could not identify a pair to buy or sell based on the text message. Make sure each text includes a pair string WITHOUT a '/', like so 'ethpax' (not case sensitive). Here is the message you sent: \n" + tradeNotification)
-          }
+          return
         }
+        email.sendTextErrorEmail("Bitmex bot could not identify a pair to buy or sell based on the text message. Make sure each text includes a pair string WITHOUT a '/', like so 'ethpax' (not case sensitive). Here is the message you sent: \n" + tradeNotification)
       }
     } else {
       email.sendTextErrorEmail("Bot could not identify whether to buy or sell based on the text message. Make sure each text includes the string 'buy' or 'sell' (not case sensitive). Here is the message you sent: \n" + tradeNotification)
